71dfbff17d9d755e20f1631a62a84fe75adb63ee
Семенова Дарья <kotik.d31sha@yandex.ru>
2023-11-28 10:56:33 +0300

    final edits

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,6 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, AllowAny
-from rest_framework.permissions import SAFE_METHODS
 from djoser.views import UserViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 
